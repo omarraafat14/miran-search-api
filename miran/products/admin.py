4ff7a1f3b3f2d4d7b38f2b99693fa2a6d138ebaf
Omar Raafat <omarraafat14@gmail.com>
2025-06-10 14:24:38 +0300

    Refactor admin.py to enhance filter functionality and improve search fields for Category and Brand models. Update list_filter in ProductAdmin to use custom filters for better usability. Update Django version in requirements to ensure compatibility and upgrade django-unfold to the latest version for improved features.

--- miran/products/admin.py
+++ miran/products/admin.py
@@ -4,6 +4,11 @@ from . import models
 from ..services.admin import BaseModelAdmin
 from modeltranslation.admin import TabbedTranslationAdmin
 from unfold.admin import StackedInline
+from unfold.contrib.filters.admin import (
+    AutocompleteSelectFilter,
+    AutocompleteSelectMultipleFilter,
+    BooleanRadioFilter,
+)
 
 
 class NutritionFactsInline(StackedInline):
@@ -14,18 +19,24 @@ class NutritionFactsInline(StackedInline):
 
 @admin.register(models.Category)
 class CategoryAdmin(BaseModelAdmin, TabbedTranslationAdmin):
-    pass
+    search_fields = ["name"]
 
 
 @admin.register(models.Brand)
 class BrandAdmin(BaseModelAdmin, TabbedTranslationAdmin):
-    pass
+    search_fields = ["name"]
 
 
 @admin.register(models.Product)
 class ProductAdmin(BaseModelAdmin, TabbedTranslationAdmin):
     list_display = ["name", "brand", "category", "price", "is_active"]
-    list_filter = ["brand", "category", "is_active"]
+    list_filter = [
+        ("brand", AutocompleteSelectFilter),
+        ("category", AutocompleteSelectMultipleFilter),
+        ("is_active", BooleanRadioFilter),
+    ]
+    list_filter_submit = True
+    list_filter_sheet = False
     search_fields = ["name", "sku"]
     search_help_text = "Search by name or sku"
     inlines = [NutritionFactsInline]
